7dfdcb95c31c91bd864ceef44750f07c43918fd3
yh2237 <2237.yh.2237@gmail.com>
2025-12-20 05:04:56 +0900

    uhe3

--- 2237yh.net/js/menu.js
+++ 2237yh.net/js/menu.js
@@ -5,11 +5,9 @@ const overlay = document.getElementById("overlay");
 menuButton.addEventListener("click", () => {
     sideMenu.classList.toggle("open");
     overlay.classList.toggle("open");
-    document.body.classList.toggle("menu-open");
 });
 
 overlay.addEventListener("click", () => {
     sideMenu.classList.remove("open");
     overlay.classList.remove("open");
-    document.body.classList.remove("menu-open");
 });
